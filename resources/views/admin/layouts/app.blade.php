d15ceb92bb5ebbaa9d0b5c70e9b738e17ec8d413
Alan Medina <alan.medina@flex.com>
2019-01-11 21:48:34 -0600

    Inital Setup

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!-- 
+    /* ==========================================================================
+    ** Main Layout Load Other Views
+    ** 11/01/{CURRENT_YEAR}
+    ** Alan Medina Silva
+    ** ========================================================================== */
+-->
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        @include('admin.layouts.head')
+    </head>
+    <body class="hold-transition skin-purple sidebar-mini">
+    <div class="wrapper">
+        @include('admin.layouts.header')
+        @include('admin.layouts.sidebar')
+        @section('main-content')
+            @show
+        @include('admin.layouts.footer')
+    </div>
+    </body>
+</html>
